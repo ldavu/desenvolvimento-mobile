dd70de1a4df7a21479701d44f1d73f9f7a3ec0ef
Davu Lima de Almeida <41762352+ldavu@users.noreply.github.com>
2023-04-11 16:52:18 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -52,3 +52,12 @@ let y = "10"; // aqui basicamente o javascript ele vai entender que apenas po´s
 let z = x / y;
 console.log(z)
 console.log('"o resultado de 100/10 é:"', z)
+
+
+
+const num1 = 1.0
+console.log(typeof num1)
+const num2 = 1.1
+console.log(typeof num2)
+console.log(Number.isInteger(num1)) // retorna TRUE
+console.log(Number.isInteger(num2)) // FALSE
